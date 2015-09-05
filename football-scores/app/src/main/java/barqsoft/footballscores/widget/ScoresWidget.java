ee1ed2e1d6f402ff4f4c5089fc89dae2ad9aa6b3
Jhoon Saravia <jhoonsar@gmail.com>
2015-09-05 09:52:14 -0500

    Created widget with static info

---
+++ football-scores/app/src/main/java/barqsoft/footballscores/widget/ScoresWidget.java
@@ -0,0 +1,58 @@
+package barqsoft.footballscores.widget;
+
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import barqsoft.footballscores.MainActivity;
+import barqsoft.footballscores.R;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class ScoresWidget extends AppWidgetProvider {
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        final int N = appWidgetIds.length;
+        for (int i = 0; i < N; i++) {
+            updateAppWidget(context, appWidgetManager, appWidgetIds[i]);
+        }
+    }
+
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.scores_widget);
+
+        views.setTextViewText(R.id.home_name, "Home");
+//        views.setContentDescription(R.id.home_name, );
+        views.setTextViewText(R.id.away_name, "Away");
+        views.setTextViewText(R.id.score_textview, "1 - 1");
+
+        //views.setTextViewText(R.id.appwidget_text, widgetText);
+
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        views.setOnClickPendingIntent(R.id.score_textview, pendingIntent);
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+}
+
